c01644a0280c3e48f25bc63bdf15cc40143600fa
kosmos-zhang <kosmos_zhang@hotmail.com>
2019-06-23 22:01:14 +0800

    Update csharp/Problem0016/Program.cs
    add the code for problem 16

--- csharp/Problem0016/Program.cs
+++ csharp/Problem0016/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Problem0016
 {
@@ -11,7 +12,35 @@ namespace Problem0016
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> v = new List<int>();
+            v.Add(1);
+
+            for (int m = 1; m <= 1000; m++)
+            {
+                int jw = 0;
+                int r = 0;
+                for (int i = 0; i < v.Count; i++)
+                {
+                    r = v[i] * 2 + jw;
+                    v[i] = (r >= 10) ? (r - 10) : r;
+                    jw = (r >= 10) ? 1 : 0;
+                }
+                if (jw > 0)
+                {
+                    v.Add(jw);
+                }
+            }
+
+            Int64 k = 0;
+            foreach(int i in v)
+            {
+                k += i;
+            }
+
+            Console.WriteLine(k);
+
+            v.Reverse();
+            Console.WriteLine(string.Concat(v));
         }
     }
 }
