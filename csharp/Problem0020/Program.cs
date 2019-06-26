7694370d92464ce2bbce16faefb1c639631e8cd0
kosmos-zhang <kosmos_zhang@hotmail.com>
2019-06-26 22:48:06 +0800

    add codes for problem 20 & 21

---
+++ csharp/Problem0020/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problem0020
+{
+    /// <summary>
+    /// n! means n × (n − 1) × ... × 3 × 2 × 1
+    /// For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+    /// and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+    /// Find the sum of the digits in the number 100!
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> res = new List<int>();
+            res.Add(1);
+
+            for (int i = 2; i < 100; i++)
+            {
+                int m = 0;
+                for (int j = 0; j < res.Count; j ++)
+                {
+                    m = res[j] * i + m;
+                    res[j] = m % 10;
+                    m = m / 10;
+                }
+                while(m > 0)
+                {
+                    res.Add(m % 10);
+                    m = m / 10;
+                }
+            }
+
+            Int64 c = 0;
+            foreach(int i in res)
+            {
+                c += i;
+            }
+
+            Console.WriteLine(c);
+        }
+    }
+}
---
+++ csharp/Problem0021/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problem0021
+{
+    /// <summary>
+    /// Let d(n) be defined as the sum of proper divisors of n (numbers less than n which divide evenly into n).
+    /// If d(a) = b and d(b) = a, where a ≠ b, then a and b are an amicable pair and each of a and b are called amicable numbers.
+    /// For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284. The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
+    /// Evaluate the sum of all the amicable numbers under 10000.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> amic = new Dictionary<int, int>();
+
+            int res = 0;
+            for (int i = 1; i<10000; i++)
+            {
+                int c = 0;
+                if (!amic.ContainsKey(i))
+                {
+                    c = SumDivisors(i);
+                    amic.Add(i, c);
+                }
+
+                if (amic.ContainsKey(c) && amic[c] == i && i != c)
+                {
+                    res += c;
+                    res += i;
+                }
+            }
+
+            Console.WriteLine(res);
+        }
+
+        static int SumDivisors(int val)
+        {
+            int c = 1;
+            for (int i = (int)Math.Sqrt(val); i > 1; i--)
+            {
+                if (val % i == 0)
+                {
+                    c += i;
+
+                    int d = val / i;
+                    if (d != i)
+                    {
+                        c += d;
+                    }
+                }
+            }
+
+            return c;
+        }
+    }
+}
