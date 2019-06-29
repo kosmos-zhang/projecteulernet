a3df5469ab8797b3541c1e3b0e3be502a9887d9f
kosmos-zhang <kosmos_zhang@hotmail.com>
2019-06-29 20:41:21 +0800

    add code for problem 25

---
+++ csharp/Problem0024/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problem0024
+{
+    /// <summary>
+    /// A permutation is an ordered arrangement of objects. For example, 3124 is one possible permutation of the digits 1, 2, 3 and 4. If all of the permutations are listed numerically or alphabetically, we call it lexicographic order. The lexicographic permutations of 0, 1 and 2 are:
+    ///   012   021   102   120   201   210
+    /// What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> lst = new List<int>();
+
+            for (int i = 0; i <= 9; i++)
+            {
+                lst.Add(i);
+            }
+
+            // begin with 0, there're 9*8*7*6*5*4*3*2*1=362880 numbers
+            // begin with 1, there're 9*8*7*6*5*4*3*2*1=362880 numbers
+            // so we calc the numbers begin with 2
+
+            // second with 0, there're 8*7*6*5*4*3*2*1= 40320
+            // second with 1, there're 8*7*6*5*4*3*2*1= 40320
+            // ...
+            // we check it second with 7, and the lower counts are: 967680
+
+            // ...  27***
+            // 7 number can have numbers = 5040
+            // so the third is 8, lower counts are 967680+5040*6=997920
+
+            // ..  278**     720,    forth is 3,  lower number counts: 999360
+            // ..  2783**    120,    fifth is 9,  lower number counts: 999960
+            // ..  27839**    24,    sixth is 1,  lower number counts: 999984
+            // ..  278391**    6,    seventh is 5, lower counts: 999996
+            // ..  2783915*    2,    eigth is 4
+
+            //     2783915460
+         
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ csharp/Problem0025/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problem0025
+{
+    /// <summary>
+    /// The Fibonacci sequence is defined by the recurrence relation:
+    /// Fn = Fn−1 + Fn−2, where F1 = 1 and F2 = 1.
+    /// Hence the first 12 terms will be:
+    /// F1 = 1
+    /// F2 = 1
+    /// F3 = 2
+    /// F4 = 3
+    /// F5 = 5
+    /// F6 = 8
+    /// F7 = 13
+    /// F8 = 21
+    /// F9 = 34
+    /// F10 = 55
+    /// F11 = 89
+    /// F12 = 144
+    /// The 12th term, F12, is the first term to contain three digits.
+    /// What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> lst1 = new List<int>();
+            List<int> lst2 = new List<int>();
+
+            lst1.Add(1);
+            lst2.Add(1);
+
+            int i = 2;
+            bool iseven = false;
+            while (lst1.Count<1000 && lst2.Count < 1000)
+            {
+                if (iseven)
+                {
+                    int j = 0;
+                    int m = 0;
+                    for (; j < lst1.Count && j<lst2.Count; j++)
+                    {
+                        int k = lst1[j] + lst2[j] + m;
+                        if (k >= 10)
+                        {
+                            lst1[j] = k - 10;
+                            m = 1;
+                        }
+                        else
+                        {
+                            lst1[j] = k;
+                            m = 0;
+                        }
+                    }
+                    while (j<lst1.Count)
+                    {
+                        int k = lst1[j] + m;
+                        if (k >= 10)
+                        {
+                            lst1[j] = k - 10;
+                            m = 1;
+                        }
+                        else
+                        {
+                            lst1[j] = k;
+                            m = 0;
+                        }
+                        j++;
+                    }
+                    while (j < lst2.Count)
+                    {
+                        int k = lst2[j] + m;
+                        if (k >= 10)
+                        {
+                            lst1.Add(k - 10);
+                            m = 1;
+                        }
+                        else
+                        {
+                            lst1.Add(k);
+                            m = 0;
+                        }
+                        j++;
+                    }
+                    if (m > 0)
+                    {
+                        lst1.Add(m);
+                    }
+                }
+                else
+                {
+                    int j = 0;
+                    int m = 0;
+                    for (; j < lst1.Count && j < lst2.Count; j++)
+                    {
+                        int k = lst1[j] + lst2[j] + m;
+                        if (k >= 10)
+                        {
+                            lst2[j] = k - 10;
+                            m = 1;
+                        }
+                        else
+                        {
+                            lst2[j] = k;
+                            m = 0;
+                        }
+                    }
+                    while (j < lst2.Count)
+                    {
+                        int k = lst2[j] + m;
+                        if (k >= 10)
+                        {
+                            lst2[j] = k - 10;
+                            m = 1;
+                        }
+                        else
+                        {
+                            lst2[j] = k;
+                            m = 0;
+                        }
+                        j++;
+                    }
+                    while (j < lst1.Count)
+                    {
+                        int k = lst1[j] + m;
+                        if (k >= 10)
+                        {
+                            lst2.Add(k - 10);
+                            m = 1;
+                        }
+                        else
+                        {
+                            lst2.Add(k);
+                            m = 0;
+                        }
+                        j++;
+                    }
+                    if (m>0)
+                    {
+                        lst2.Add(m);
+                    }
+                }
+
+                iseven = !iseven;
+                i++;
+            }
+
+            Console.WriteLine(i);
+        }
+    }
+}
