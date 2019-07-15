28df590b098974aa38c5afb4ccf1b9033b27ccdf
kosmos-zhang <kosmos_zhang@hotmail.com>
2019-07-15 22:54:51 +0800

    add code for problem 31

--- csharp/Problem0031/Program.cs
+++ csharp/Problem0031/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Problem0031
 {
@@ -13,7 +14,53 @@ namespace Problem0031
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] a = new int[] { 1, 2, 5, 10, 20, 50, 100, 200 };
+
+            GetCount(200, a, 7, "0");
+
+            Console.WriteLine(sumLst.Count);
+        }
+
+        static List<string> sumLst = new List<string>();
+        private static void GetCount(int sum, int[] a, int v, string cur)
+        {
+            if (sum == 0)
+            {
+                if (!sumLst.Contains(cur))
+                {
+                    sumLst.Add(cur);
+                    Console.WriteLine(sumLst.Count + "\t" + cur);
+                }
+                return;
+            }
+
+            string tmp = "";
+            if (v == 0)
+            {
+                tmp = string.Format("{0}+{1}*{2}", cur, sum / 1, 1);
+                if (!sumLst.Contains(tmp))
+                {
+                    sumLst.Add(tmp);
+                    Console.WriteLine(sumLst.Count + "\t" + tmp);
+                }
+                return;
+            }
+
+
+            for (int j = v;j>0;j--)
+            {
+                int m = sum / a[j];
+                int n = sum % a[j];
+
+                for (int i = m; i > 0; i--)
+                {
+                    tmp = string.Format("{0}+{1}*{2}", cur, i, a[j]);
+                    GetCount(sum - i * a[j], a, j - 1, tmp);
+                }
+
+                tmp = string.Format("{0}", cur);
+                GetCount(sum, a, j - 1, tmp);
+            }
         }
     }
 }
